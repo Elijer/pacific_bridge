62af381cc9cf131054fc9a4141cc29409ff2fddd
Elijah Kennedy <elijahclimbs@gmail.com>
2024-07-14 11:39:06 -0400

    hide cursor yellow bg syncing state between cursors

--- server/server.js
+++ server/server.js
@@ -1,50 +1,73 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 import multer from 'multer';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import cors from 'cors';
+import { __dirname } from './utils.js'; // Import the helper function
 
 const app = express();
-app.use(cors()); // Enable CORS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*', // Allow all origins for simplicity, adjust as needed
+    methods: ['GET', 'POST']
+  }
+});
 
-// Set up multer for file uploads
+let currentFile = null
+
+app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname(import.meta.url), 'uploads')));
+
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, 'uploads/');
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
   }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
-// Existing routes and middleware...
-
-// New route for handling 3D model uploads (GLTF and GLB)
 app.post('/upload-3d-model', upload.single('file'), (req, res) => {
-  console.log(req.file)
   if (!req.file) {
-    console.log("NO FILE UPLOADED")
     return res.status(400).json({ error: 'No file uploaded' });
   }
 
   const fileExtension = path.extname(req.file.originalname).toLowerCase();
   if (fileExtension !== '.gltf' && fileExtension !== '.glb') {
-    console.log("INVALID FILE TYPE")
     return res.status(400).json({ error: 'Invalid file type. Only GLTF and GLB files are allowed.' });
   }
 
+  const fileUrl = `http://localhost:3000/uploads/${req.file.filename}`;
+  currentFile = fileUrl
   res.json({
     message: '3D model file uploaded successfully',
-    filename: req.file.filename
+    fileUrl: fileUrl
+  });
+
+  // Broadcast the file URL to all connected clients
+  io.emit('modelUploaded', { fileUrl });
+});
+
+io.on('connection', (socket) => {
+  if (currentFile) {
+    socket.emit('modelUploaded', { fileUrl: currentFile });
+  }
+  console.log('a user connected');
+
+  // Handle cursor position events
+  socket.on('cursorPosition', (data) => {
+    socket.broadcast.emit('cursorPosition', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
